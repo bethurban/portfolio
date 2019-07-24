178fbf3871f211cbc4c1b8c410a63219c149eec7
bethurban <beth.urban@gmail.com>
2019-07-24 18:37:39 -0400

    Update colors to navy scheme.

--- src/Colors.js
+++ src/Colors.js
@@ -1,11 +1,11 @@
 const Colors = {
-    white: '#ffffff',
-    light: '#f6f1ed',
-    lightGrey: '#e5dada',
-    dark: '#b5a397',
-    darker: '#6a5750',
-    darkest: '#30231d',
-    closeIcon: '#969ea4'
-  }
-  
-  export default Colors;
+  white: '#ffffff',
+  light: '#E7E9ED',
+  lightGrey: 'D0D4DC',
+  dark: '#748096',
+  darker: '#01173F',
+  darkest: '#010F29',
+  closeIcon: '#969ea4'
+};
+
+export default Colors;
